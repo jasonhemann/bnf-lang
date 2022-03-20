953cd52531439830d4e45e7c0207ac206382416b
Jason Hemann <jason.hemann@gmail.com>
2022-03-20 12:53:41 -0400

    W/Michael correcting the phasing issues

--- bnf-lang.rkt
+++ bnf-lang.rkt
@@ -1,9 +1,9 @@
 #lang racket/base
 
 (require ee-lib/define
-         ee-lib
-         syntax/parse
          (for-syntax
+          ee-lib
+          syntax/parse
           racket/base))
 
 (define-literal-forms bnf-literals
@@ -13,20 +13,23 @@
    #%literal
    #%non-literal])
 
-;;;;;;;; structure types for variable and macro binding
+(begin-for-syntax
+  ;;;;;;;; structure types for variable and macro binding
 
-(struct rule-binding-rep [])
-(struct grammar-binding-rep [])
-(struct rule-macro-rep [])
-(struct grammar-macro-rep [])
+  (struct rule-binding-rep [])
+  (struct grammar-binding-rep [])
+  (struct rule-macro-rep [])
+  (struct grammar-macro-rep [])
 
-;;;;;; template of the expander
+  ;;;;;; template of the expander
 
-(define/hygienic (expand-make-rule stx) #:definition
-  (syntax-parse stx
-    #:literal-sets (bnf-literals)
-    #:literals (quote)
-    [(_ rule-name:id rewrite ...+)
-     ;; check that rule-name hasn't been taken
-     ;; check that rule is not name of grammar
-     #`(make-rule rule-name (stx-map expand-term rewrite) ...)]))
+  (define/hygienic (expand-make-rule stx) #:definition
+    (syntax-parse stx
+      #:literal-sets (bnf-literals)
+      #:literals (quote)
+      [(_ rule-name:id rewrite ...+)
+       ;; check that rule-name hasn't been taken
+       ;; check that rule is not name of grammar
+       #`(make-rule rule-name (stx-map expand-term rewrite) ...)])))
+
+
